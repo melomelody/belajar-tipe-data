649ded73cdcf8dff6e37c7645f39c3166bd00778
melomelody <melomelody.gm@gmail.com>
2018-01-03 11:21:46 +0700

    Belajar Tipe Data 2

---
+++ Index.java
@@ -0,0 +1,9 @@
+public class Index{
+    public static void main(String[] args){
+        System.out.println("a : " + TipeData.nama);
+        System.out.println("b : " + TipeData.aktif);
+        System.out.println("c : " + TipeData.GolDarah);
+        System.out.println("d : " + TipeData.umur);
+        System.out.println("e : " + TipeData.hehe);
+    }
+}
---
+++ TipeData.java
@@ -0,0 +1,7 @@
+public class TipeData{
+    public static final String nama = "Melodita";
+    public static final Boolean aktif = true;
+    public static final Character GolDarah = 'O';
+    public static final Integer umur = 22;
+    public static final Double hehe = 30.599;  
+}
