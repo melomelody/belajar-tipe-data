afbc77d5a9551b14842110694a2d51d988df04d8
melomelody <melomelody.gm@gmail.com>
2018-01-03 10:12:19 +0700

    Belajar Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+
+    public static void main(String[] args) {
+        System.out.println("Hello ini program Java pertama");
+        System.out.println("Hari Kedua");
+
+        Integer angkaSatu = 10;
+        System.out.println(angkaSatu);
+    }
+
+
+}
